9407f558c92dda71bfd7b034d0afba364cc8252c
= <=>
2020-02-26 20:57:23 -0300

    libreria commons con las models entity

---
+++ commons-models/src/test/java/com/practica/microservicios/commons/models/commonsmodels/CommonsModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.microservicios.commons.models.commonsmodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
